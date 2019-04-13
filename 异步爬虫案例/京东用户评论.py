a042c27285362da6b11aca1e1d00494c49901e67
BennyJane <45121489+BennyJane@users.noreply.github.com>
2019-04-13 14:07:39 +0800

    Update 京东用户评论.py

--- 异步爬虫案例/京东用户评论.py
+++ 异步爬虫案例/京东用户评论.py
@@ -5,6 +5,7 @@ import asyncio
 import pandas as pd
 import logging
 import json
+import time
 import random
 
 # 设置日志格式
@@ -28,14 +29,13 @@ async def fetch(sem, session, url):
 
 # 解析网页
 async def parser(html,url):
-    #获取字典形式的数据
+
     try:
         content = re.findall(r'\((.*)\)', html)[0]
     except Exception as e:
         print(url)
         print(e)
-    
-    #将字符串转化为字典形式
+
     result = json.loads(content)
     # print(comment)
     for i in result["comments"]:
@@ -72,117 +72,42 @@ async def download(sem, url):
 
 
 if __name__=="__main__":
-    #每个商品的评论信息分为5类
-
     # 统计该爬虫的消耗时间
     print('*' * 50)
     t3 = time.time()
 
-    # 抓取视频
-    max_page =7
-    urls=['https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv6306&productId=7301584&score=7&sortType=5&page=%d&pageSize=10&isShadowSku=0&fold=1'
-          %i for i in range(0,max_page)]
+    item_name='华为手环b5_商务版'
+    # 一次性复制五种品论的url链接
+    urls=[
+        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv1504&productId=30764834196&score=7&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1',
+        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv1504&productId=30764834196&score=5&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1',
+        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv1504&productId=30764834196&score=3&sortType=6&page=0&pageSize=10&isShadowSku=0&fold=1',
+        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv1504&productId=30764834196&score=2&sortType=6&page=0&pageSize=10&isShadowSku=0&fold=1',
+        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv1504&productId=30764834196&score=1&sortType=6&page=0&pageSize=10&isShadowSku=0&fold=1',
+    ]
+    # 注意url、页面、视频名称的匹配
+    Names = ['视频', '追加', '好评', '中评', '差评']
 
-    # 利用asyncio模块进行异步IO处理
-    loop = asyncio.get_event_loop()
-    sem = asyncio.Semaphore(2)
-    tasks = [asyncio.ensure_future(download(sem, url)) for url in urls]
-    tasks = asyncio.gather(*tasks)
-    loop.run_until_complete(tasks)
+    # 设置各类评论爬取数量
+    max_pages = [20, 10, 100, 2, 3]
 
-    #df.to_excel('E:/Dada/华为B3.xls')
-    df.to_csv('E:\Dada\乐心手环3_视频.csv')
-    print("01已爬完")
+    i=0
+    for url in urls:
+        #替换页面符号
+        url=url.replace('page=0', 'page=%d')
+        urls=[url %i for i in range(0,max_pages[i])]
+        # 利用asyncio模块进行异步IO处理
+        loop = asyncio.get_event_loop()
+        sem = asyncio.Semaphore(2)
+        tasks = [asyncio.ensure_future(download(sem, url)) for url in urls]
+        tasks = asyncio.gather(*tasks)
+        loop.run_until_complete(tasks)
 
-    #抓取追加评论
-    rows_num_01=df.shape[0]+1
-    max_page = 10
-    urls = [
-        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv6306&productId=7301584&score=5&sortType=5&page=%d&pageSize=10&isShadowSku=0&fold=1'
-        % i for i in range(0, max_page)]
-
-    # 利用asyncio模块进行异步IO处理
-    loop = asyncio.get_event_loop()
-    sem = asyncio.Semaphore(2)
-    tasks = [asyncio.ensure_future(download(sem, url)) for url in urls]
-    tasks = asyncio.gather(*tasks)
-    loop.run_until_complete(tasks)
-
-    #选择新增数据，并生成新的表
-    newDf_01 = pd.DataFrame(df.loc[rows_num_01:, :])
-
-    newDf_01.to_csv('E:\Dada\乐心手环3_追加.csv')
-    print("02已爬完")
-
-    #抓取好评
-    rows_num_02=df.shape[0]+1
-
-    max_page = 100
-    urls = [
-        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv6306&productId=7301584&score=3&sortType=6&page=%d&pageSize=10&isShadowSku=0&fold=1'
-        % i for i in range(0, max_page)]
-
-    # 利用asyncio模块进行异步IO处理
-    loop = asyncio.get_event_loop()
-    sem = asyncio.Semaphore(2)
-    tasks = [asyncio.ensure_future(download(sem, url)) for url in urls]
-    tasks = asyncio.gather(*tasks)
-    loop.run_until_complete(tasks)
-
-    #选择新增数据，并生成新的表
-    newDf_02 = pd.DataFrame(df.loc[rows_num_02:, :])
-
-    newDf_02.to_csv('E:\Dada\乐心手环3_好评.csv')
-    print("03已爬完")
-
-    #抓取中评
-    rows_num_03=df.shape[0]+1
-
-    max_page = 5
-    urls = [
-        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv6306&productId=7301584&score=2&sortType=6&page=%d&pageSize=10&isShadowSku=0&fold=1'
-        % i for i in range(0, max_page)]
-
-    # 利用asyncio模块进行异步IO处理
-    loop = asyncio.get_event_loop()
-    sem = asyncio.Semaphore(2)
-    tasks = [asyncio.ensure_future(download(sem, url)) for url in urls]
-    tasks = asyncio.gather(*tasks)
-    loop.run_until_complete(tasks)
-
-    #选择新增数据，并生成新的表
-    newDf_03 = pd.DataFrame(df.loc[rows_num_03:, :])
-
-    newDf_03.to_csv('E:\Dada\乐心手环3_中评.csv')
-    print("04已爬完")
-
-    #抓取差评
-    rows_num_04=df.shape[0]+1
-
-    max_page = 5
-    urls = [
-        'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv6306&productId=7301584&score=1&sortType=6&page=%d&pageSize=10&isShadowSku=0&fold=1'
-        % i for i in range(0, max_page)]
-
-    # 利用asyncio模块进行异步IO处理
-    loop = asyncio.get_event_loop()
-    sem = asyncio.Semaphore(2)
-    tasks = [asyncio.ensure_future(download(sem, url)) for url in urls]
-    tasks = asyncio.gather(*tasks)
-    loop.run_until_complete(tasks)
-
-    #选择新增数据，并生成新的表
-    newDf_04 = pd.DataFrame(df.loc[rows_num_04:, :])
-
-    newDf_04.to_csv('E:\Dada\乐心手环3_差评.csv')
-    print("05已爬完")
+        PathName = r"E:\Dada\%s_%s.csv'" %(item_name, Names[i])
+        df.to_csv(PathName)
+        print("第%s部分已爬完" %Names[] )
+        i=i+1
 
     #记录总时间
     t4 = time.time()
     print('总共耗时：%s' % (t4 - t3))
-
-
-
-
-
-
