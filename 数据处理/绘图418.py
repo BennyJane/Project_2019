74e51fe48a63c68fa34282a3ce6d0bebd90ef28b
BennyJane <45121489+BennyJane@users.noreply.github.com>
2019-04-18 20:59:21 +0800

    Update 绘图418.py

--- 数据处理/绘图418.py
+++ 数据处理/绘图418.py
@@ -9,11 +9,18 @@ from datetime import datetime
 import matplotlib.dates as mdates
 from dateutil import parser
 
+                #读取数据，提取数据
+
+
+
+
+
+
 #控制图片大小、分辨率；只设置一个子图
 plt.figure(figsize=(13,8), dpi=80)
 plt.subplot(1,1,1)
 
-#绘制第一段数据图形
+                  #绘制第一段数据图形  
 
 #提取第一段数据的时间序列;并将其解析为时间类型数据
 data01_date_str = first_df.iloc[:, 1] #选择单一列数据
@@ -25,6 +32,41 @@ data01_mean=first_df.iloc[:, 2]
 plt.plot(data01_date, data01_mean, color='k', linewidth=0.8, label='predict')
 
 
+                  #绘制第二段数据图形  
+#导入第二段代码处理的数据
+Left_df_filepath="E:/编程接单/2019-4-14/Second/Left_df01.csv"
+Right_df_filepath="E:/编程接单/2019-4-14/Second/Right_df01.csv"
+#导入左列数据，并处理日期
+Left_df=pd.read_csv(Left_df_filepath)
+end_num=Left_df.shape[0]
+for i in range(0,end_num):
+    Left_df.iloc[i,0]=Left_df.iloc[i,0][5:19]
+
+#导入右列数据，并处理日期
+Right_df=pd.read_csv(Right_df_filepath)
+end_num = Right_df.shape[0]
+for i in range(0, end_num):
+    Right_df.iloc[i, 0] = Right_df.iloc[i, 0][5:19]
+
+
+#排除两者不等长的情况，取较小的列
+if Left_df.shape[0]!=Right_df.shape[0]:
+    if Left_df.shape[0]>Right_df.shape[0]:
+        nums=Right_df.shape[0]
+    else:
+        nums = Left_df.shape[0]
+else:
+    nums=Left_df.shape[0]
+
+#先将左右两端数据合并到一个df文件中（可以改进合并的方式）
+Simple_Col=pd.DataFrame(columns=['exchange_time','bid/ask_price'])
+for i in range(0,nums):
+    Simple_Col = Simple_Col.append(Left_df.iloc[i,:], ignore_index=True)
+    Simple_Col = Simple_Col.append(Right_df.iloc[i,:], ignore_index=True)
+
+Simple_nums=Simple_Col.shape[0]
+for j in range(0,Simple_nums):
+  
 
 
 
@@ -32,7 +74,6 @@ plt.plot(data01_date, data01_mean, color='k', linewidth=0.8, label='predict')
 
 # 显示图示
 plt.legend()
-
 
 # 配置横坐标
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
