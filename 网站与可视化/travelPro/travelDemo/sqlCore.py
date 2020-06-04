7d903083ce1586d74f8f9bbc02f854cca659eb5d
Benny Jane <2314255424@qq.com>
2020-06-04 08:12:48 +0800

    绘制旅游网的图片

--- 网站与可视化/travelPro/travelDemo/sqlCore.py
+++ 网站与可视化/travelPro/travelDemo/sqlCore.py
@@ -29,9 +29,11 @@ def getSite():
     allSite = site_db.select_sql(["*"], sqlFilter=f'order by num_comment desc limit 12')
     return allSite
 
+
 def getManySite():
     allSite = site_db.select_sql(["*"], sqlFilter=f'where img != "" limit 16')
     return allSite
+
 
 def getHotel():
     allHotel = hotel_db.select_sql(["*"], sqlFilter=f'order by price desc limit 16')
@@ -41,3 +43,32 @@ def getHotel():
 def getContent(id):
     site = site_db.select_sql(["*"], sqlFilter=f'where id = "{id}"')
     return site
+
+
+class sqlBase:
+
+    def topFirst(self):
+        # 获取评论数量前十的景区
+        ten_site = site_db.select_sql(['site_name', 'num_comment'], sqlFilter=f"order by num_comment desc limit 10")
+        ten_site = sorted(ten_site, key=lambda x: int(x['num_comment']), reverse=True)
+        # 获取评分最高的10个酒店
+        ten_hotel = hotel_db.select_sql(['hotel_name', 'comment_num'], sqlFilter=f"order by comment_num desc limit 10")
+        ten_hotel = sorted(ten_hotel, key=lambda x: int(x['comment_num']), reverse=True)
+        # 获取游记数量最多的10个地区
+        ten_site_info = site_db.select_sql(['site_name', 'num_ginfo'], sqlFilter=f"order by num_ginfo desc limit 10")
+        ten_site_info = sorted(ten_site_info, key=lambda x: int(x['num_ginfo']), reverse=True)
+        return ten_site, ten_hotel, ten_site_info
+
+    def picData(self):
+        ten_site = site_db.select_sql(['site_name', 'num_comment', 'num_ginfo'],
+                                      sqlFilter=f"order by num_comment desc limit 14")
+        ten_site = sorted(ten_site, key=lambda x: int(x['num_comment']), reverse=True)
+        res = []
+        for item in ten_site:
+            temp = {
+                'name': item['site_name'],
+                'value1': item['num_comment'],
+                'value2': item['num_ginfo'],
+            }
+            res.append(temp)
+        return res
